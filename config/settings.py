8e17d1d989240d6bc4c2064f642fbe145735c979
sorena <mdn1376@gmail.com>
2024-07-16 20:44:59 +0330

    feat: add app account
    added this app for managing users

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     'apps.home.apps.HomeConfig',
+    'apps.account.apps.AccountConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.home.urls', namespace='home')),
+    path('account/', include('apps.account.urls', namespace='account')),
 ]
