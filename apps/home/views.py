6d8873504fa0f4f35b8c22591bfec8a48d8bebdb
sorena <mdn1376@gmail.com>
2024-07-18 18:24:33 +0330

    feat: add PostDeleteView
    added view for delete posts and use in templates
    
    this function in just for user who made the post

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from apps.home.models import Post
 
@@ -25,3 +27,17 @@ class PostDetailView(View):
             'post': post
         }
         return render(request, self.template_name, context)
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        try:
+            post = Post.objects.get(pk=post_id)
+            if post.user.id == request.user.id:
+                post.delete()
+                messages.success(request, 'Post deleted successfully', extra_tags='success')
+            else:
+                messages.error(request, 'You are not authorized to delete this post', extra_tags='error')
+        except Post.DoesNotExist:
+            messages.warning(request, 'Post does not exist', extra_tags='warning')
+        return redirect('home:home')
