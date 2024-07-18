0f3af04fc671d9f14fc719d58f985abe6dfa6740
sorena <mdn1376@gmail.com>
2024-07-18 16:43:56 +0330

    feat: made PostDetailView
    made view for post detail & use in html pages

--- apps/home/views.py
+++ apps/home/views.py
@@ -11,3 +11,17 @@ class HomeView(View):
         posts = Post.objects.all()
         context = {'posts': posts}
         return render(request, self.template_name, context)
+
+
+class PostDetailView(View):
+    template_name = 'home/post_detail.html'
+
+    def get(self, request, post_id, post_slug):
+        try:
+            post = Post.objects.get(pk=post_id, slug=post_slug)
+        except Post.DoesNotExist:
+            post = None
+        context = {
+            'post': post
+        }
+        return render(request, self.template_name, context)
