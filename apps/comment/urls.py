748b41a89edc9c34721d873aacca0866245f4c1e
sorena <mdn1376@gmail.com>
2024-07-21 14:42:24 +0330

    feat: added ReplyCommentView
    added this view for reply to a comment & use in post_detail.html

--- apps/comment/urls.py
+++ apps/comment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from apps.comment import views
 
 app_name = 'comment'
 
 urlpatterns = [
-
+    path('reply/<int:post_id>/<int:comment_id>/', views.ReplyCommentView.as_view(), name='reply_comment'),
 ]
--- apps/comment/views.py
+++ apps/comment/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
+from django.views import View
 
-# Create your views here.
+from apps.comment.forms import CreateCommentForm
+from apps.comment.models import Comment
+from apps.home.models import Post
+
+
+class ReplyCommentView(LoginRequiredMixin, View):
+    form_class = CreateCommentForm
+
+    def post(self, request, post_id, comment_id):
+        post = get_object_or_404(Post, pk=post_id)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.user = request.user
+            reply.post = post
+            reply.reply = comment
+            reply.is_reply = True
+            reply.save()
+            messages.success(request, 'your reply submitted successfully', extra_tags='success')
+        return redirect('home:post_detail', post_id=post.id, post_slug=post.slug)
