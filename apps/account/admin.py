fd641bac36e7856a64b52c4364971910d301cd2b
sorena <mdn1376@gmail.com>
2024-07-22 13:08:12 +0330

    feat: change Profile admin
    register profile admin inline to user base model

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from apps.account.models import Relation, Profile
 
@@ -8,6 +10,14 @@ class RelationModelAdmin(admin.ModelAdmin):
     list_display = ('from_user', 'to_user')
 
 
-@admin.register(Profile)
-class ProfileModelAdmin(admin.ModelAdmin):
-    list_display = ('user', 'age', 'bio')
+class ProfileInline(admin.StackedInline):
+    can_delete = False
+    model = Profile
+
+
+class ExtendedUserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, ExtendedUserAdmin)
