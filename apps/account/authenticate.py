eb9cb6a2f5094bd32bb46f85c1acbade5847fb6a
sorena <mdn1376@gmail.com>
2024-08-11 12:00:57 +0330

    feat: added comments for EmailBackend

--- apps/account/authenticate.py
+++ apps/account/authenticate.py
@@ -3,8 +3,15 @@ from django.contrib.auth.models import User
 
 
 class EmailBackend(BaseBackend):
+    """
+    custom authentication backend that allows users to log in using their email address
+    """
+
+    # The `authenticate` method is called to validate a user based on the provided credentials.
+
     def authenticate(self, request, username=None, password=None):
         try:
+            # Attempt to retrieve a user based on the provided email address (stored in `username`)
             user = User.objects.get(email=username)
             if user.check_password(password):
                 return user
@@ -12,6 +19,7 @@ class EmailBackend(BaseBackend):
         except User.DoesNotExist:
             return None
 
+    # The `get_user` method is used to retrieve a user object based on their user ID
     def get_user(self, user_id):
         try:
             return User.objects.get(pk=user_id)
